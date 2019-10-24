40be20088f4a28443a8f18da384ec388685686e3
Isadora de Menezes <isadoranezes@gmail.com>
2019-10-24 14:59:37 -0300

    [Recommit] Remocao de conexao com o CloudMQTT

--- pse_smus/pse_smus.ino
+++ pse_smus/pse_smus.ino
@@ -1,17 +1,10 @@
 #include <ESP8266WiFi.h>  
 #include <Wire.h>
-#include <PubSubClient.h>
 
 //defines
 #define SSID_REDE     "teste" 
 #define SENHA_REDE    "12345678" 
 #define INTERVALO_ENVIO_THINGSPEAK  30000  
-
-const char* BROKER_MQTT = "m16.cloudmqtt.com";
-int BROKER_PORT         = 17996;
-const char* mqttUser = "povafjjv";              //user
-const char* mqttPassword = "2yxCAb-waqJv";      //password
-const char* mqttTopicSub ="teste_smus";            //tópico que sera assinado
 
 //constantes e variáveis globais
 char EnderecoAPIThingSpeak[] = "api.thingspeak.com";
@@ -24,41 +17,6 @@ void EnviaInformacoesThingspeak(String StringDados);
 void FazConexaoWiFi(void);
 float FazLeituraUmidade(void);
 
-void initMQTT();
-PubSubClient mqtt(client);
-/*
- * Implementações
- */
-void initMQTT() {
-  mqtt.setServer(BROKER_MQTT, BROKER_PORT);
-  mqtt.setCallback(mqtt_callback);
-}
-
-void mqtt_callback(char* topic, byte* payload, unsigned int length) {
-
-  String message;
-  for (int i = 0; i < length; i++) {
-    char c = (char)payload[i];
-    message += c;
-  }
-  Serial.println("Tópico => " + String(topic) + " | Valor => " + String(message));
-  Serial.flush();
-}
-
-void reconnectMQTT() {
-  while (!mqtt.connected()) {
-    Serial.println("Tentando se conectar ao Broker MQTT: " + String(BROKER_MQTT));
-    if (mqtt.connect("ESP8266Client", mqttUser, mqttPassword)) {
-      Serial.println("Conectado");
-      mqtt.subscribe("teste_smus");
-
-    } else {
-      Serial.println("Falha ao Reconectar");
-      Serial.println("Tentando se reconectar em 2 segundos");
-      delay(2000);
-    }
-  }
-}
 //Função: envia informações ao ThingSpeak
 //Parâmetros: String com a  informação a ser enviada
 //Retorno: nenhum
@@ -74,7 +32,6 @@ void EnviaInformacoesThingspeak(String StringDados){
         client.print(StringDados.length());
         client.print("\n\n");
         client.print(StringDados);
-
         lastConnectionTime = millis();
         Serial.println("- Informações enviadas ao ThingSpeak!");
      }   
@@ -112,10 +69,6 @@ float FazLeituraUmidade(void){
      Serial.print("[Umidade Percentual] ");
      Serial.print(UmidadePercentual);
      Serial.println("%");
-
-     char umidade[4];
-     dtostrf(ValorADC, 2, 2, umidade);
-     mqtt.publish("teste_smus", umidade);
  
      return UmidadePercentual;
 }
@@ -124,12 +77,10 @@ void setup(){
     lastConnectionTime = 0; 
     FazConexaoWiFi();
     Serial.println("Monitoramento de Umidade de Solo");
-    initMQTT();
 }
 
 //loop principal
 void loop(){
-    
     float UmidadePercentualLida;
     int UmidadePercentualTruncada;
     char FieldUmidade[11];
@@ -140,7 +91,6 @@ void loop(){
         Serial.println("- Desconectado do ThingSpeak");
         Serial.println();
     }
-
     UmidadePercentualLida = FazLeituraUmidade();
     UmidadePercentualTruncada = (float)UmidadePercentualLida; //trunca umidade como número inteiro
 
@@ -150,10 +100,5 @@ void loop(){
         sprintf(FieldUmidade,"field1=%d",UmidadePercentualTruncada);
         EnviaInformacoesThingspeak(FieldUmidade);
     }
-
-     delay(1000);
-    if (!mqtt.connected()) {
-      reconnectMQTT();
-    }
-    mqtt.loop();
+    delay(1000);
 }
